73e46989fed62e32cbec5f95d822d6abfd54765f
hellbergAndreas <hellberg.andreas@gmail.com>
2020-03-26 22:34:03 +0100

    a

--- admin/db.php
+++ admin/db.php
@@ -18,6 +18,7 @@
   $db_username  = "root";
   $db_password  = "root";
 
+<<<<<<< HEAD
   try {
       $db = new PDO("mysql:host=$db_server;dbname=$db_database;charset=utf8", $db_username, $db_password);
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
