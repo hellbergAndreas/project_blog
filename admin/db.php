e2f2c7f893557f3bb347791b36d19ec5f9dc9283
hellbergAndreas <hellberg.andreas@gmail.com>
2020-03-26 22:35:47 +0100

    merging
    trying to solve merge conflict

--- admin/db.php
+++ admin/db.php
@@ -18,23 +18,9 @@
   $db_username  = "root";
   $db_password  = "root";
 
-<<<<<<< HEAD
   try {
       $db = new PDO("mysql:host=$db_server;dbname=$db_database;charset=utf8", $db_username, $db_password);
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $e) {
       echo "<h2>Error: " .$e->getMessage(). "</h2";
   }
-=======
-  try{
-    $db = new PDO("mysql:host=$db_server;dbname=$db_database;charset=utf8"
-                  , $db_username
-                  , $db_password);
-    $db->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
-
-    // echo "<h2>Connected Successfully</h2>";
-    
-  } catch(PDOException $e){
-    echo "<h2>Error: " .$e->getMessage(). "</h2";
-  }
->>>>>>> 54a67185c7d439a62859cc6f6809e05e1dcb6914
